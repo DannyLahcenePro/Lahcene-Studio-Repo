2477ac470999bc940b7ee08850f817b044459926
DannyLahcenePro <danny.lahcene.pro@gmail.com>
2025-01-06 17:42:45 +0100

    Delete assets directory

--- assets/contact-DZMUjAHX.js
+++
@@ -1,2 +0,0 @@
-import"./main-D0qWa0L8.js";import{T as i}from"./three-animation-DnUW4nHC.js";window.threeAnimation=new i("flow");
-//# sourceMappingURL=contact-DZMUjAHX.js.map
--- assets/contact-DcjUX7W5.js
+++
@@ -1,2 +0,0 @@
-import"./main-B_bRBhR6.js";import{T as i}from"./three-animation-DnUW4nHC.js";window.threeAnimation=new i("flow");
-//# sourceMappingURL=contact-DcjUX7W5.js.map
